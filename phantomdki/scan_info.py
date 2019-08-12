03676981da064958fd30ff5908d76a40cae446c9
Tristan Kuehn <tristankuehn@gmail.com>
2019-08-12 10:44:02 -0400

    Clean up and test scan_info

--- phantomdki/scan_info.py
+++ phantomdki/scan_info.py
@@ -1,10 +1,10 @@
-import math
+"""A set of classes used to describe a phantom dataset."""
 
-DKI_METRICS = ['fa', 'md', 'ad', 'rd', 'mk', 'ak', 'rk']
+import math
 
 class Phantom:
     def __init__(self, hotend_temp, print_speed, layer_thickness,
-            infill_density, infill_pattern):
+                 infill_density, infill_pattern):
         self.hotend_temp = hotend_temp
         self.print_speed = print_speed
         self.layer_thickness = layer_thickness
@@ -22,4 +22,43 @@ class SingleScan:
     def __init__(self, name, tube_slice):
         self.name = name
         self.tube_slice = tube_slice
+
+class ParallelLinePattern:
+    def __init__(self, cura_angle):
+        self.cura_angle = cura_angle
+
+    def get_direction(self, point):
+        return 90 - self.cura_angle
+
+class ConcentricArcPattern:
+    def __init__(self, origin):
+        self.origin = origin
+
+    def get_direction(self, point):
+        displacement = (self.origin[0] - point[0], self.origin[1] - point[1])
+        if displacement[0] == 0:
+            return 0
+
+        disp_angle = math.degrees(math.atan(displacement[1] / displacement[0]))
+
+        if disp_angle > 0:
+            return disp_angle - 90
+        else:
+            return disp_angle + 90
+
+class AlternatingPattern:
+    def __init__(self, pattern_0, pattern_1):
+        self.pattern_0 = pattern_0
+        self.pattern_1 = pattern_1
+
+    def get_crossing_angle(self, point):
+        patterns = [self.pattern_0, self.pattern_1]
+        directions = [pattern.get_direction(point) for pattern in patterns]
+
+        crossing_angle = max(directions) - min(directions)
+
+        if crossing_angle > 90:
+            return 180 - crossing_angle
+        else:
+            return crossing_angle
 
---
+++ test/test_scan_info.py
@@ -0,0 +1,35 @@
+from phantomdki import scan_info
+import unittest
+
+class TestConcentricArcPattern(unittest.TestCase):
+    def setUp(self):
+        self.pattern = scan_info.ConcentricArcPattern((0, 0))
+
+    def test_get_direction(self):
+        self.assertEqual(self.pattern.get_direction((0, 0)), 0)
+        self.assertEqual(self.pattern.get_direction((1, 0)), 90)
+        self.assertEqual(self.pattern.get_direction((0, 1)), 0)
+        self.assertEqual(self.pattern.get_direction((-1, 0)), 90)
+        self.assertEqual(self.pattern.get_direction((0, -1)), 0)
+        self.assertEqual(self.pattern.get_direction((1, 1)), -45)
+        self.assertEqual(self.pattern.get_direction((1, -1)), 45)
+        self.assertEqual(self.pattern.get_direction((-1, 1)), 45)
+        self.assertEqual(self.pattern.get_direction((-1, -1)), -45)
+
+class TestParallelLinePattern(unittest.TestCase):
+    def setUp(self):
+        self.pattern = scan_info.ParallelLinePattern(0)
+
+    def test_get_direction(self):
+        self.assertEqual(self.pattern.get_direction((1, 1)), 90)
+
+class TestAlternatingPattern(unittest.TestCase):
+    def setUp(self):
+        self.pattern = scan_info.AlternatingPattern(
+                scan_info.ParallelLinePattern(0),
+                scan_info.ConcentricArcPattern((0, 0)))
+
+    def test_get_crossing_angle(self):
+        self.assertEqual(self.pattern.get_crossing_angle((0, 0)), 90)
+        self.assertEqual(self.pattern.get_crossing_angle((1, 0)), 0)
+
