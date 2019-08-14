29e0cd66197de60ff72d6e5f4907725d34722708
Tristan Kuehn <tristankuehn@gmail.com>
2019-08-14 16:20:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="phantomdki",
+    name="dmriphantomutils",
     version="0.0.1",
     author="Tristan Kuehn",
     author_email="tkuehn@uwo.ca",
     description="DKI analysis for diffusion phantoms",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude="test"),
     install_requires=['dipy', 'matplotlib', 'numpy', 'scikit-image', 'scipy']
 )
 
