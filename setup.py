4646a58968140823a468e955107e60207f2c15b1
Tristan Kuehn <tristankuehn@gmail.com>
2019-08-14 14:37:41 -0400

    Update requirements

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires=['nibabel', 'dipy', 'skimage']
+    install_requires=['dipy', 'matplotlib', 'numpy', 'scikit-image', 'scipy']
 )
 
