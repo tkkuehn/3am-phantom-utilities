40e1276b7fa46cdb1b0265dfa34fe15458a90fe5
Tristan Kuehn <tristankuehn@gmail.com>
2019-08-15 11:33:49 -0400

    Split out more functionality in fit_dki

--- dmriphantomutils/fit_dki.py
+++ dmriphantomutils/fit_dki.py
@@ -9,7 +9,7 @@ import scipy.ndimage as ndi
 
 from dmriphantomutils import image_io
 
-def fit_dki(dkimodel, dwi, blur=False):
+def fit_dki(dwi, blur=False):
     """Fit a DKI model to a DWI, applying a mask if provided.
 
     Parameters
@@ -24,6 +24,7 @@ def fit_dki(dkimodel, dwi, blur=False):
     dwifit
         A fit from which parameter maps can be generated
     """
+    dkimodel = dki.DiffusionKurtosisModel(dwi.gtab)
 
     data = dwi.getImage()
     
@@ -37,9 +38,37 @@ def fit_dki(dkimodel, dwi, blur=False):
 
     return dkimodel.fit(data, mask)
 
+def save_metric_imgs(
+        dwi, dkifit, fa_path=None, md_path=None, ad_path=None,
+        rd_path=None, mk_path=None, ak_path=None, rk_path=None):
+    source_affine = dwi.img.affine
+    source_header = dwi.img.header
+
+    # Should think about theoretical min and max kurtosis values for us
+    if fa_path is not None:
+        image_io.save_image(dkifit.fa, source_affine, source_header, fa_path)
+
+    if md_path is not None:
+        image_io.save_image(dkifit.md, source_affine, source_header, md_path)
+
+    if ad_path is not None:
+        image_io.save_image(dkifit.ad, source_affine, source_header, ad_path)
+
+    if rd_path is not None:
+        image_io.save_image(dkifit.rd, source_affine, source_header, rd_path)
+
+    if mk_path is not None:
+        image_io.save_image(dkifit.mk(), source_affine, source_header, mk_path)
+
+    if ak_path is not None:
+        image_io.save_image(dkifit.ak(), source_affine, source_header, ak_path)
+
+    if rk_path is not None:
+        image_io.save_image(dkifit.rk(), source_affine, source_header, rk_path)
+
 def main(nifti_path, bval_path, bvec_path, mask_path=None, blur=False,
-        fa_path=None, md_path=None, ad_path=None, rd_path=None, mk_path=None,
-        ak_path=None, rk_path=None):
+         fa_path=None, md_path=None, ad_path=None, rd_path=None, mk_path=None,
+         ak_path=None, rk_path=None):
     """Load and fit an image to a DKI model, then save its parameters.
 
     This is meant to deal with the functionality of this module being called as
@@ -73,33 +102,11 @@ def main(nifti_path, bval_path, bvec_path, mask_path=None, blur=False,
 
     dwi = image_io.load_dwi(nifti_path, bval_path, bvec_path, mask_path)
 
-    dkimodel = dki.DiffusionKurtosisModel(dwi.gtab)
-    dkifit = fit_dki(dkimodel, dwi, blur)
+    dkifit = fit_dki(dwi, blur)
 
-    source_affine = dwi.img.affine
-    source_header = dwi.img.header
-
-    # Should think about theoretical min and max kurtosis values for us
-    if fa_path is not None:
-        image_io.save_image(dkifit.fa, source_affine, source_header, fa_path)
-
-    if md_path is not None:
-        image_io.save_image(dkifit.md, source_affine, source_header, md_path)
-
-    if ad_path is not None:
-        image_io.save_image(dkifit.ad, source_affine, source_header, ad_path)
-
-    if rd_path is not None:
-        image_io.save_image(dkifit.rd, source_affine, source_header, rd_path)
-
-    if mk_path is not None:
-        image_io.save_image(dkifit.mk(), source_affine, source_header, mk_path)
-
-    if ak_path is not None:
-        image_io.save_image(dkifit.ak(), source_affine, source_header, ak_path)
-
-    if rk_path is not None:
-        image_io.save_image(dkifit.rk(), source_affine, source_header, rk_path)
+    save_metric_imgs(dwi, dkifit, fa_path=fa_path, md_path=md_path,
+                     ad_path=ad_path, rd_path=rd_path, mk_path=mk_path,
+                     ak_path=ak_path, rk_path=rk_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=
@@ -118,6 +125,6 @@ if __name__ == "__main__":
     parser.add_argument('--rk')
     args = parser.parse_args()
     main(args.nifti, args.bval, args.bvec, args.mask, blur=args.blur,
-            fa_path=args.fa, md_path=args.md, ad_path=args.ad, rd_path=args.rd,
-            mk_path=args.mk, ak_path=args.ak, rk_path=args.rk)
+         fa_path=args.fa, md_path=args.md, ad_path=args.ad, rd_path=args.rd,
+         mk_path=args.mk, ak_path=args.ak, rk_path=args.rk)
 
