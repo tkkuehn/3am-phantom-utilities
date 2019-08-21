d181d128641f0fbe41af484a2c90d9706d3deaca
Tristan Kuehn <tristankuehn@gmail.com>
2019-08-21 14:15:08 -0400

    Restructure infill pattern interface
    This will allow proper duck typing

--- dmriphantomutils/scan_info.py
+++ dmriphantomutils/scan_info.py
@@ -2,6 +2,8 @@
 
 import math
 
+import numpy as np
+
 class Phantom:
     def __init__(self, hotend_temp, print_speed, layer_thickness,
                  infill_density, infill_pattern):
@@ -13,7 +15,7 @@ class Phantom:
 
 class WaterSlice:
     def __init__(self):
-        pass
+        self.infill_pattern = EmptyPattern() 
 
 class Study:
     def __init__(self, name, tube, sessions):
@@ -30,18 +32,32 @@ class SingleScan:
     def __init__(self, tube_slice):
         self.tube_slice = tube_slice
 
+class EmptyPattern:
+    def __init__(self):
+        pass
+
+    def get_geometry_generators(self):
+        return {}
+
 class ParallelLinePattern:
     def __init__(self, cura_angle):
         self.cura_angle = cura_angle
 
-    def get_direction(self, point):
+    def get_geometry_generators(self):
+        return {'direction': self._get_direction}
+
+    def _get_direction(self, point):
         return 90 - self.cura_angle
 
 class ConcentricArcPattern:
     def __init__(self, origin):
         self.origin = origin
 
-    def get_direction(self, point):
+    def get_geometry_generators(self):
+        return {'direction': self._get_direction,
+                'arc_radius': self._get_arc_radius}
+
+    def _get_direction(self, point):
         displacement = (self.origin[0] - point[0], self.origin[1] - point[1])
         if displacement[0] == 0:
             return 0
@@ -53,14 +69,24 @@ class ConcentricArcPattern:
         else:
             return disp_angle + 90
 
+    def _get_arc_radius(self, point):
+        displacement = np.array(
+            [self.origin[0] - point[0], self.origin[1] - point[1]])
+
+        return np.linalg.norm(displacement)
+
 class AlternatingPattern:
     def __init__(self, pattern_0, pattern_1):
         self.pattern_0 = pattern_0
         self.pattern_1 = pattern_1
 
-    def get_crossing_angle(self, point):
+    def get_geometry_generators(self):
+        return {'crossing_angle': self._get_crossing_angle}
+
+    def _get_crossing_angle(self, point):
         patterns = [self.pattern_0, self.pattern_1]
-        directions = [pattern.get_direction(point) for pattern in patterns]
+        directions = [pattern.get_geometry_generators()['direction'](point)
+            for pattern in patterns]
 
         crossing_angle = max(directions) - min(directions)
 
--- test/test_scan_info.py
+++ test/test_scan_info.py
@@ -7,22 +7,28 @@ class TestConcentricArcPattern(unittest.TestCase):
         self.pattern = scan_info.ConcentricArcPattern((0, 0))
 
     def test_get_direction(self):
-        self.assertEqual(self.pattern.get_direction((0, 0)), 0)
-        self.assertEqual(self.pattern.get_direction((1, 0)), 90)
-        self.assertEqual(self.pattern.get_direction((0, 1)), 0)
-        self.assertEqual(self.pattern.get_direction((-1, 0)), 90)
-        self.assertEqual(self.pattern.get_direction((0, -1)), 0)
-        self.assertEqual(self.pattern.get_direction((1, 1)), -45)
-        self.assertEqual(self.pattern.get_direction((1, -1)), 45)
-        self.assertEqual(self.pattern.get_direction((-1, 1)), 45)
-        self.assertEqual(self.pattern.get_direction((-1, -1)), -45)
+        get_direction = self.pattern.get_geometry_generators()['direction']
+        self.assertEqual(get_direction((0, 0)), 0)
+        self.assertEqual(get_direction((1, 0)), 90)
+        self.assertEqual(get_direction((0, 1)), 0)
+        self.assertEqual(get_direction((-1, 0)), 90)
+        self.assertEqual(get_direction((0, -1)), 0)
+        self.assertEqual(get_direction((1, 1)), -45)
+        self.assertEqual(get_direction((1, -1)), 45)
+        self.assertEqual(get_direction((-1, 1)), 45)
+        self.assertEqual(get_direction((-1, -1)), -45)
+
+    def test_get_arc_radius(self):
+        get_arc_radius = self.pattern.get_geometry_generators()['arc_radius']
+        self.assertEqual(get_arc_radius((3, 4)), 5)
 
 class TestParallelLinePattern(unittest.TestCase):
     def setUp(self):
         self.pattern = scan_info.ParallelLinePattern(0)
 
     def test_get_direction(self):
-        self.assertEqual(self.pattern.get_direction((1, 1)), 90)
+        get_direction = self.pattern.get_geometry_generators()['direction']
+        self.assertEqual(get_direction((1, 1)), 90)
 
 class TestAlternatingPattern(unittest.TestCase):
     def setUp(self):
@@ -31,6 +37,8 @@ class TestAlternatingPattern(unittest.TestCase):
                 scan_info.ConcentricArcPattern((0, 0)))
 
     def test_get_crossing_angle(self):
-        self.assertEqual(self.pattern.get_crossing_angle((0, 0)), 90)
-        self.assertEqual(self.pattern.get_crossing_angle((1, 0)), 0)
+        get_crossing_angle = (
+            self.pattern.get_geometry_generators()['crossing_angle'])
+        self.assertEqual(get_crossing_angle((0, 0)), 90)
+        self.assertEqual(get_crossing_angle((1, 0)), 0)
 
