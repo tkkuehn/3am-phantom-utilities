7d3331ae3b7d2d997bd1700e763303041ee4dde2
Tristan Kuehn <tristankuehn@gmail.com>
2019-12-04 09:49:58 -0500

    Simplify automask a bit

--- dmriphantomutils/automask.py
+++ dmriphantomutils/automask.py
@@ -9,8 +9,14 @@ import numpy as np
 import skimage.filters as filters
 import skimage.morphology as mm
 
-def mask_bubbles(slice_b0):
+def mask_phantom(slice_b0):
     """Generate a mask for the phantom material in a slice.
+
+    Assuming the phantom's diameter is similar to the test tube's,
+    Otsu's method segments the phantom well. The initial results are
+    eroded to avoid outliers near the edges of the phantom.
+
+    Big enough air bubbles may also be masked out by this function.
 
     Parameters
     ----------
@@ -25,10 +31,7 @@ def mask_bubbles(slice_b0):
     """
     slice_b0_threshold = filters.threshold_otsu(slice_b0)
     slice_phantom_mask = slice_b0 > slice_b0_threshold
-    slice_phantom_data = slice_b0[mm.binary_erosion(
-        slice_phantom_mask, mm.disk(3)).nonzero()]
-    slice_bubble_threshold = filters.threshold_otsu(slice_phantom_data)
-    return slice_b0 > slice_bubble_threshold
+    return mm.binary_erosion(slice_phantom_mask, mm.disk(3))
 
 def main(nifti_path, slice_idx, mask_output):
     """Load an image and mask one z-slice.
@@ -47,17 +50,17 @@ def main(nifti_path, slice_idx, mask_output):
     img_b0 = img_data[:, :, :, 0]
 
     slice_b0 = img_b0[:, :, slice_idx]
-    slice_bubble_mask = mask_bubbles(slice_b0)
+    slice_phantom_mask = mask_phantom(slice_b0)
 
     out_data = np.zeros(img_b0.shape)
-    out_data[:, :, slice_idx] = slice_bubble_mask
+    out_data[:, :, slice_idx] = slice_phantom_mask
 
     mask_img = nib.nifti1.Nifti1Image(out_data, img.affine, header=img.header)
     nib.save(mask_img, mask_output)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=
-            'Load a nifti image and mask the bubbles from one slice.')
+            'Load a nifti image and mask the phantom from one slice.')
     parser.add_argument('nifti')
     parser.add_argument('z_slice', type=int)
     parser.add_argument('output')
