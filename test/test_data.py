e97d207fd0c408001750046b5d1a5fdb660eccfb
Tristan Kuehn <tristankuehn@gmail.com>
2019-12-05 10:44:20 -0500

    Remove test dependence on image files

--- test/test_data.py
+++ test/test_data.py
@@ -1,24 +1,39 @@
 import os.path
 import tempfile
 
+from dipy.core.gradients import GradientTable
 import numpy as np
 
-TEST_RESOURCES = os.path.join('test', 'resources')
+class MockDiffusionWeightedImage():
+    def __init__(self, data=None, gtab=None, mask=None):
+        if data is None:
+            data = np.random.uniform(100, 300, size=(50, 50, 3, 100))
+        if gtab is None:
+            b0s = 20 * np.ones([20, 1])
+            b1000s = 1020 * np.ones([80, 1])
+            bvals = np.append(b0s, b1000s, 0)
+            np.random.shuffle(bvals)
 
-def get_data_nifti():
-    return os.path.join(TEST_RESOURCES, 'test.nii')
+            phis = np.random.uniform(0, 2 * np.pi, (100, 1))
+            thetas = np.arccos(np.random.uniform(-1, 1, (100, 1)))
+            x = bvals * np.sin(thetas) * np.cos(phis)
+            y = bvals * np.sin(thetas) * np.cos(phis)
+            z = bvals * np.cos(thetas)
+            gradients = np.append(x, np.append(y, z, 1), 1)
 
-def get_data_bval():
-    return os.path.join(TEST_RESOURCES, 'test.bval')
+            gtab = GradientTable(gradients)
+        if mask is None:
+            mask = np.ones([50, 50, 3])
 
-def get_data_bvec():
-    return os.path.join(TEST_RESOURCES, 'test.bvec')
+        self.data = data
+        self.mask = mask
+        self.gtab = gtab
 
-def get_data_mask_nifti():
-    return os.path.join(TEST_RESOURCES, 'test_mask.nii.gz')
+    def getImage(self):
+        return self.data
 
-def get_temp_output_nifti():
-    return os.path.join(tempfile.gettempdir(), 'test.nii.gz')
+    def getFlatData(self):
+        return self.data.flatten()
 
 class MockDerivedImage():
     def __init__(self, data=None, mask=None):
--- test/test_dipy_fit.py
+++ test/test_dipy_fit.py
@@ -1,15 +1,13 @@
+import os.path
+import tempfile
 import unittest
 
 from dmriphantomutils import dipy_fit
 from test import test_data
 
 class TestDipyFit(unittest.TestCase):
-    def testMain(self):
-        test_nifti_path = test_data.get_data_nifti()
-        test_bval_path = test_data.get_data_bval()
-        test_bvec_path = test_data.get_data_bvec()
-        test_mask_path = test_data.get_data_mask_nifti()
-        test_fa_path = test_data.get_temp_output_nifti()
-        dipy_fit.main(test_nifti_path, test_bval_path, test_bvec_path,
-                     mask_path=test_mask_path, blur=True, fa_path=test_fa_path)
+    def testFitDki(self):
+        dwi = test_data.MockDiffusionWeightedImage()
+
+        dkifit = dipy_fit.fit_dki(dwi)
 
