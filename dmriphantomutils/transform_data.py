6e2597d68bcf07392915480b02d470e8c44cbfc9
Tristan Kuehn <tristankuehn@gmail.com>
2019-12-10 11:15:37 -0500

    Allow use of fiducials for image transformation

--- dmriphantomutils/transform_data.py
+++ dmriphantomutils/transform_data.py
@@ -10,6 +10,8 @@ negative y-axis. Then a translation and rotation can move each voxel's
 coordinates from image space to ground truth space.
 """
 
+import warnings
+
 import numpy as np
 from skimage.morphology import binary_closing, disk
 from skimage.measure import label, regionprops
@@ -33,7 +35,7 @@ def find_centroid(mask):
     closed_mask = label(binary_closing(mask, disk(6)))
     return regionprops(closed_mask)[0].centroid
 
-def transform_image_point(point, centroid, angle):
+def transform_image_point(point, centroid, angle=None, fiducial=None):
     """Perform a rigid transform of a given point.
 
     The infill pattern definitions assume the origin is at the centroid
@@ -46,11 +48,15 @@ def transform_image_point(point, centroid, angle):
     ----------
     point : tuple of int
         The indices of the point to be transformed in image space
-    centroid : tuple of int
+    centroid : tuple of float
         The indices of the phantom's centroid in image space
-    angle : float
+    angle : float, optional
         The angle by which the phantom would need to be rotated to have
-        the fiducial at the bottom in the x-y plane
+        the fiducial at the bottom in the x-y plane. Exactly one of
+        angle or fiducial must be included as an argument.
+    fiducial : tuple of float, optional
+        The location of the fiducial in image space. Exactly one of
+        fiducial or angle must be included as an argument.
 
     Returns
     -------
@@ -58,6 +64,18 @@ def transform_image_point(point, centroid, angle):
         The corresponding indices of the original point in ground truth
         space.
     """
+
+    if angle is not None and fiducial is not None:
+        # Should only provide one. Default to using angle...
+        warnings.warn('Both angle ({}) and fiducial ({}) were provided to'
+            + 'transform_image_point. Defaulting to angle.'.format(
+                angle, fiducial))
+    elif fiducial is not None:
+        translated_fiducial = np.array(fiducial) - np.array(centroid)
+        angle = -90 - np.degrees(
+            np.arctan2(translated_fiducial[1], translated_fiducial[0]))
+    elif angle is None:
+        raise TypeError("Exactly one of angle or fiducial is required.")
 
     translated_point = np.array(point) - np.array(centroid)
 
@@ -69,7 +87,8 @@ def transform_image_point(point, centroid, angle):
 
     return (rotated_point[0, 0], rotated_point[1, 0])
 
-def gen_geometry_data(mask_data, geometry_generator, centroid, angle, scaling):
+def gen_geometry_data(mask_data, geometry_generator, centroid, scaling,
+        angle=None, fiducial=None):
     """Generate geometric ground truth data from a mask.
 
     Parameters
@@ -80,11 +99,15 @@ def gen_geometry_data(mask_data, geometry_generator, centroid, angle, scaling):
         Function to get the quantity of interest given a point.
     centroid : tuple of int
         Centroid of the phantom in image space.
-    angle : float
-        The angle by which the phantom would need to be rotated to have
-        the fiducial at the bottom in the x-y plane.
     scaling : float
         Isotropic scale factor from coords to image space.
+    angle : float, optional
+        The angle by which the phantom would need to be rotated to have
+        the fiducial at the bottom in the x-y plane. Exactly one of
+        angle or fiducial must be included as an argument.
+    fiducial : tuple of float, optional
+        The location of the fiducial in image space. Exactly one of
+        fiducial or angle must be included as an argument.
 
     Returns
     -------
@@ -92,13 +115,29 @@ def gen_geometry_data(mask_data, geometry_generator, centroid, angle, scaling):
         An image of the calculated geometry data
     """
 
+    use_fiducial = False
+    if angle is not None and fiducial is not None:
+        # Should only provide one. Default to using angle...
+        warnings.warn('Both angle ({}) and fiducial ({}) were provided to'
+            + 'gen_geometry_data. Defaulting to angle.'.format(
+                angle, fiducial))
+        fiducial = None
+    elif fiducial is not None:
+        use_fiducial = True
+    elif angle is None:
+        raise TypeError("Exactly one of angle or fiducial is required.")
+
     geometry_data = np.zeros(mask_data.shape)
 
     for z in range(mask_data.shape[2]):
         for m_idx, m_val in np.ndenumerate(mask_data[..., z]):
             if m_val:
-                transformed = transform_image_point(
-                        m_idx, centroid, angle)
+                if use_fiducial:
+                    transformed = transform_image_point(
+                        m_idx, centroid, fiducial=fiducial)
+                else:
+                    transformed = transform_image_point(
+                        m_idx, centroid, angle=angle)
                 transformed = (transformed[0] * scaling,
                                transformed[1] * scaling)
                 ground_truth = geometry_generator(transformed)
--- test/test_transform_data.py
+++ test/test_transform_data.py
@@ -1,4 +1,5 @@
 import unittest
+import warnings
 
 import numpy as np
 
@@ -21,13 +22,41 @@ class TestTransformData(unittest.TestCase):
 
     def test_transform_image_point(self):
         self.assertAlmostEqual(
-                transform_data.transform_image_point((3, 3), (2, 2), 0)[0], 1)
+            transform_data.transform_image_point(
+                (3, 3), (2, 2), angle=0)[0], 1)
         self.assertAlmostEqual(
-                transform_data.transform_image_point((3, 3), (2, 2), 0)[1], 1)
+            transform_data.transform_image_point(
+                (3, 3), (2, 2), fiducial=(2, 1))[0], 1)
         self.assertAlmostEqual(
-                transform_data.transform_image_point((3, 0), (2, 0), 90)[0], 0)
+            transform_data.transform_image_point(
+                (3, 3), (2, 2), angle=0)[1], 1)
         self.assertAlmostEqual(
-                transform_data.transform_image_point((3, 0), (2, 0), 90)[1], 1)
+            transform_data.transform_image_point(
+                (3, 3), (2, 2), fiducial=(2, 1))[1], 1)
+        self.assertAlmostEqual(
+            transform_data.transform_image_point(
+                (3, 0), (2, 0), angle=90)[0], 0)
+        self.assertAlmostEqual(
+            transform_data.transform_image_point(
+                (3, 0), (2, 0), fiducial=(1, 0))[0], 0)
+        self.assertAlmostEqual(
+            transform_data.transform_image_point(
+                (3, 0), (2, 0), angle=90)[1], 1)
+        self.assertAlmostEqual(
+            transform_data.transform_image_point(
+                (3, 0), (2, 0), fiducial=(1, 0))[1], 1)
+
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter('always')
+            self.assertAlmostEqual(
+                transform_data.transform_image_point(
+                    (3, 3), (2, 2), angle=0, fiducial=(1, 2))[0], 1)
+            self.assertEqual(len(w), 1)
+            self.assertEqual(str(w[0].message)[:4], 'Both')
+
+        with self.assertRaises(TypeError):
+            transform_data.transform_image_point((3, 3), (2, 2))
+
 
     def test_compare_to_pattern(self):
         img = test_data.MockDerivedImage()
@@ -37,8 +66,17 @@ class TestTransformData(unittest.TestCase):
         centroid = (15, 15)
         angle = 45
 
-        pattern_r = transform_data.gen_geometry_data(
-                mask, get_arc_radius, centroid, angle, 1)
+        with self.assertRaises(TypeError):
+            pattern_r = transform_data.gen_geometry_data(
+                mask, get_arc_radius, centroid, 1)
+
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter('always')
+            pattern_r = transform_data.gen_geometry_data(
+                mask, get_arc_radius, centroid, 1, angle=angle,
+                fiducial=(15, 0))
+            self.assertEqual(len(w), 1)
+            self.assertEqual(str(w[0].message)[:4], 'Both')
 
         self.assertAlmostEqual(pattern_r[15, 15, 2], 0)
 
